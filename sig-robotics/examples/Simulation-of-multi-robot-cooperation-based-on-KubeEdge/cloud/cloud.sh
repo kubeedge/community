f9cd3a52fcc2ac4208d52c4c45b860dbbd83aac2
jiangfan wu <jiangfanwu@stu.xjtu.edu.cn>
2022-10-31 23:52:36 +0800

    add docker image and modefiy readme
    Signed-off-by: jiangfan wu <jiangfanwu@stu.xjtu.edu.cn>

---
+++ sig-robotics/examples/Simulation-of-multi-robot-cooperation-based-on-KubeEdge/cloud/cloud.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#script to run with a host network
+#export ROS_MASTER_URI=http://$(hostname --ip-address):11311
+#export ROS_HOSTNAME=$(hostname --ip-address)
+# setup ros environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "${PRJ_DIR}/devel/setup.bash"
+roslaunch tuw_multi_robot_demo Cloud.launch room:=cave nr_of_robots:=3
--- sig-robotics/examples/Simulation-of-multi-robot-cooperation-based-on-KubeEdge/edge_ws/src/tuw_multi_robot_edge/tuw_multi_robot_demo/README.md
+++
@@ -1,4 +0,0 @@
-# tuw\_multi\_robot\_demo
-Contains launch and config files to run a sample demo. 
-## Tutorials
-- [demo using three robots with stage](tutorials/demo01.md)
